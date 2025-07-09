127eeec511a7922c211b5e78daa00de6ea3791f6
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2025-07-09 12:39:34 -0400

    header layout made

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="navbar navbar-expand-lg bg-dark">
+        <div></div>
+
+        <div>
+          <h1>Memory Card Game</h1>
+          <h3>Try not to click any card twice!</h3>
+        </div>
+        <div>
+          <h4>High Score: 0</h4>
+          <h4>Current Score: 0</h4>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
